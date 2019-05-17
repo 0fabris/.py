f7eac60845d59c6cf611643d2eb4f2ded7a36385
fabris <49992867+0fabris@users.noreply.github.com>
2019-05-17 21:27:04 +0200

    Create TrenitaliaInfos.py

---
+++ TrenitaliaInfos.py
@@ -0,0 +1,53 @@
+ '''
+
+    0fabris
+    Trenitalia Info Percorso
+    Programmato durante un viaggio su un Frecciarossa
+    Testato durante il viaggio
+
+    Requisiti: requests, json
+    Mi raccomando => Connessione a WifiFrecce, anche senza essere autenticati per internet
+
+'''
+
+import os,requests,json,time
+
+class TrenitaliaInfos():
+    def __init__(self):
+        self.urlbase = 'http://d6o.portalefrecce.it/PortaleFrecce/'
+        self.infosurl = self.urlbase + 'infoViaggioActionJson'
+        self.mapurl = self.urlbase + 'mapAction.action?width=590&height=282'
+        self.timeout = 5
+        self.Infos()
+
+    def Infos(self):
+        while True:
+            os.system("clear")
+            infos = json.loads(requests.get(self.infosurl).text)
+            coord = [infos["lat"],infos["lon"]]
+            try:
+                print('\n\n\n\nBuongiorno\n\nStiamo viaggiando sul treno '+ infos["categoria"]+ ' sulla tratta "' + infos["tracktitle"].replace("&rarr;", '=>') + '", num '+ infos["tracknum"]+ \
+                    '\nDirezione: '+ infos["dir"]+ \
+                    '\nAltitudine: '+ infos["alt"] + ' m s.l.m' \
+                    '\nVelocita\': '+ infos["speed"]+ " km/h" +\
+                    '\nLatitudine: '+ coord[0] + \
+                    '\nLongitudine: '+ coord[1] + \
+                    '\n'+ ("Ritardo di " +infos["delay"].split(' ')[1].replace('m','') + " minuti." if(infos["delay"] != '')else 'Nessun ritardo sulla linea.')  +\
+                    '\n')
+                print('Partiti da '+ infos["trackline"]["start"]["staz"] + " alle " + infos["trackline"]["start"]["orario"]+ \
+                '\nProssima fermata: '+ infos["trackline"]["mid"]["staz"] + " per l'orario " + infos["trackline"]["mid"]["orario"]+ \
+                '\nFermata successiva: '+ str(infos["trackline"]["end"]["staz"]) + " per l'orario " + str(infos["trackline"]["end"]["orario"]))
+                print('Progresso per la prossima fermata: {0: .2f}%'.format(infos["trackline"]["postreno"] *2))
+            except:
+                pass
+
+            print("\n\tFermate della linea:")
+            for i in infos["statoPercorso"]:
+                print(str(i['id']+1)+ ': ' + i['description'] + (" gia passata." if i['passed'] else " non passata."))
+            time.sleep(self.timeout)
+
+if __name__ == '__main__':
+    try:
+        TrenitaliaInfos()
+    except KeyboardInterrupt:
+        print("\nGrazie e Arrivederci!")
